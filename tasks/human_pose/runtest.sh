26237eda7d368124f8664ab7bc3e3fd42ccf52a0
Jetson User <jetson-rw@mail2you.net>
2020-05-28 03:24:11 +0800

    finishd goto wrist

--- tasks/human_pose/runtest.sh
+++ tasks/human_pose/runtest.sh
@@ -1,7 +1,7 @@
 export DISPLAY=:0
 
-#python3 get_keypoint_from_video.py --model=resnet --video='v4l2src io-mode=2 device=/dev/video1 ! video/x-raw, format=YUY2, width=1920, height=1080, framerate=60/1 !  nvvidconv ! video/x-raw(memory:NVMM), format=(string)I420 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink sync=false async=false drop=true'
+python3 get_keypoint_from_video.py --model=resnet --video='v4l2src io-mode=2 device=/dev/video1 ! video/x-raw, format=YUY2, width=1920, height=1080, framerate=60/1 !  nvvidconv ! video/x-raw(memory:NVMM), format=(string)I420 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink sync=false async=false drop=true'
 
 
 
-python3 get_keypoint_from_video.py --model=resnet --video='v4l2src io-mode=2 device=/dev/video1 ! image/jpeg, width=1920, height=1080, framerate=30/1 ! nvjpegdec ! video/x-raw !  nvvidconv ! video/x-raw(memory:NVMM), format=(string)I420 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink sync=false async=false drop=true'
+#python3 get_keypoint_from_video.py --model=resnet --video='v4l2src io-mode=2 device=/dev/video1 ! image/jpeg, width=1920, height=1080, framerate=30/1 ! nvjpegdec ! video/x-raw !  nvvidconv ! video/x-raw(memory:NVMM), format=(string)I420 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink sync=false async=false drop=true'
